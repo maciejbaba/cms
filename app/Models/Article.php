bb3c6ad6dedae1d0d0e79508e7a15b3549ffd691
Maciej Bąba <maciek2baba2@gmail.com>
2024-08-07 23:14:50 +0200

    create article model, create articels migation, add filament and implement with it article form

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,12 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 // by default it will use articles table
 class Article extends Model
 {
-    use HasFactory;
-    use HasUuids;
+  use HasFactory, HasUuids;
 
-    const PUBLICATION_DATE = 'creation_date';
+  protected $table = 'articles';
+
+  protected $fillable = ['title', 'body', 'user_id'];
+
+  public function author(): BelongsTo
+  {
+    return $this->belongsTo(User::class);
+  }
 }
