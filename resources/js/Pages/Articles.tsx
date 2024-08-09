f7605ca5d49413cc2cba1afcd8702e32fa750e2f
Maciej Bąba <maciek2baba2@gmail.com>
2024-08-09 10:33:04 +0200

    add shadcn ui react library

--- resources/js/Pages/Articles.tsx
+++ resources/js/Pages/Articles.tsx
@@ -1,8 +1,30 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import { PageProps } from "@/types";
+import { useEffect, useState } from "react";
+
+export type Article = {
+    id: number;
+    title: string;
+    body: string;
+    user_id: number;
+    created_at: string;
+    updated_at: string;
+};
+
+type Articles = Article[];
 
 export default function Articles({ auth }: PageProps) {
+    const [articles, setArticles] = useState<Articles>([]);
+    useEffect(() => {
+        fetch("/api/articles")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setArticles(data);
+            });
+    }, []);
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -18,7 +40,17 @@ export default function Articles({ auth }: PageProps) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            Articles
+                            {articles.map((article) => (
+                                <div
+                                    key={article.id}
+                                    className="p-6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
+                                >
+                                    <div>Title: {article.title}</div>
+                                    <div className="p-6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                                        Body: {article.body}
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
