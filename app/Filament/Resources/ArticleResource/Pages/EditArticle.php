73709912a1de71d48677a4d3e2c5f2511e448827
Maciej Bąba <maciek2baba2@gmail.com>
2024-08-10 02:20:10 +0200

    prevent editing article if not owner on filament

--- app/Filament/Resources/ArticleResource/Pages/EditArticle.php
+++ app/Filament/Resources/ArticleResource/Pages/EditArticle.php
@@ -7,10 +7,32 @@ use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Auth\Access\Response;
+use Filament\Notifications\Actions\Action;
+use Filament\Notifications\Notification;
+
 
 class EditArticle extends EditRecord
 {
   protected static string $resource = ArticleResource::class;
+
+  protected function beforeSave(): void
+  {
+
+    $article_user_id = $this->record->user_id;
+    $curr_user_id = Auth::id();
+
+    if ($article_user_id != $curr_user_id) {
+      Notification::make()
+        ->warning()
+        ->title('You are not the owner of this article.')
+        ->body('You must be the owner of the article to edit it.')
+        ->persistent()
+        ->actions([])
+        ->send();
+      $this->halt();
+      return;
+    }
+  }
 
   protected function getHeaderActions(): array
   {
