ec12eeac6276707e1084071935552c0446ecb466
Maciej Bąba <maciek2baba2@gmail.com>
2024-08-10 15:20:02 +0200

    add deleting article if you are owner

--- resources/js/Pages/ArticlePage.tsx
+++ resources/js/Pages/ArticlePage.tsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 import type { ArticleT } from "@/Components/Article";
 import { buttonVariants } from "@/components/ui/button";
 import { Link } from "@inertiajs/react";
+import { Button } from "@/components/ui/button";
+import { stat } from "fs";
 
 export default function ArticlePage({ auth }: PageProps) {
     const [article, setArticle] = useState<ArticleT>();
@@ -23,6 +25,27 @@ export default function ArticlePage({ auth }: PageProps) {
             });
     }, []);
 
+    const handleDelete = async () => {
+        const csrfToken = await fetch("/api/csrf-token").then((res) =>
+            res.text(),
+        );
+
+        const res = await fetch(`/api/articles/${articleId}`, {
+            method: "DELETE",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-Token": csrfToken,
+            },
+        });
+
+        if (res.ok) {
+            window.location.href = "/articles";
+        } else {
+            alert("Something went wrong");
+        }
+    };
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -37,15 +60,23 @@ export default function ArticlePage({ auth }: PageProps) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="flex flex-col gap-5 p-6 text-gray-900 dark:text-gray-100">
-                            <Link
-                                href={`/articles/edit/${articleId}`}
-                                className={`${buttonVariants({
-                                    variant: "outline",
-                                })} w-20`}
-                            >
-                                Edit
-                            </Link>
-
+                            <div className="flex gap-5">
+                                <Link
+                                    href={`/articles/edit/${articleId}`}
+                                    className={`${buttonVariants({
+                                        variant: "outline",
+                                    })} w-20`}
+                                >
+                                    Edit
+                                </Link>
+                                <Button
+                                    className="w-20"
+                                    variant="destructive"
+                                    onClick={handleDelete}
+                                >
+                                    Delete
+                                </Button>
+                            </div>
                             {article && <Article article={article}></Article>}
                         </div>
                     </div>
