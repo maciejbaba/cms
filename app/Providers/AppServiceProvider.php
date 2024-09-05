d2e147c571c26c58b1f2403a44adeeea89e5a5de
Maciej Bąba <maciek2baba2@gmail.com>
2024-09-05 23:00:41 +0200

    change to production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,10 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot(): void
   {
-    if ($this->app->environment('local')) {
+    if ($this->app->environment('production')) {
         URL::forceScheme('https');
     }
+
     //
   }
 }
